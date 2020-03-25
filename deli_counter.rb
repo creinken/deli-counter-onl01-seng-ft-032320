44a6c098dab3924a34af57901eab47620c9e6a89
Christopher Reinken <christopherreinken@gmail.com>
2020-03-25 16:27:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 def line(katz_deli)
   if katz_deli.length > 0 
-    current_line = katz_deli.each_with_index{|name, idx| " #{idx + 1}. #{name}"}
+    current_line = ""
+    katz_deli.each_with_index{|name, idx| 
+      current_line << " "
+      current_line << (idx + 1)
+      current_line << ". "
+      current_line << name
+    }
     puts "The line is currently:#{current_line}"
   else 
     puts "The line is currently empty."
