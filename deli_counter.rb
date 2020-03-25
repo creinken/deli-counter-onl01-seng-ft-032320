a450f05ab44b15320375be5dcf9abf7e066c75eb
Christopher Reinken <christopherreinken@gmail.com>
2020-03-25 15:39:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(katz_deli)
-  katz_deli.length > 0 ? puts katz_deli.each_with_index{|name, idx| "#{idx+1}. #{name}"
+  katz_deli.length > 0 ? puts katz_deli.each_with_index{|name, idx| "#{idx+1}. #{name}" : puts "The line is currently empty."
 end
 
 def take_a_number(katz_deli)
