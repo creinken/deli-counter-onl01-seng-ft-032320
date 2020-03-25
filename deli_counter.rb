ac84e39b9223b9a10a554dddd9fa38971ccd8b16
Christopher Reinken <christopherreinken@gmail.com>
2020-03-25 15:56:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number 1 inline."
   else
     puts "Welcome, #{name}. You are number #{katz_deli.length} inline."
+  end
 end
 
 def now_serving(katz_deli)
